8db814d540114286ef62dce875e9e2167c772388
Michal Galant <Michal Galant>
2016-09-21 21:42:16 +0200

    services implementation

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,3 +1,13 @@
 /**
  * Created by micha on 20.09.2016.
  */
+import { Injectable } from '@angular/core';
+import {Hero} from "./hero";
+import {HEROES} from "./mock-heroes";
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+}
--- app/hero.ts
+++ app/hero.ts
@@ -1,3 +1,7 @@
 /**
  * Created by micha on 20.09.2016.
  */
+export class Hero {
+  id: number;
+  name: string;
+}
